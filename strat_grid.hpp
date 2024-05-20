742a986f36d424467b72d202f12c82a07e04fee0
Licu Mihai <licu.mihai21@gmail.com>
2024-05-21 01:14:52 +0300

    merged

--- strat_grid.hpp
+++ strat_grid.hpp
@@ -10,9 +10,8 @@
 #include "vec3.hpp"
 #include "viewport.hpp"
 #include <mpi.h>
-#include <string>
 #include <vector>
-enum Strategy { HORIZONTAL, GRID };
+enum Strategy { HORIZONTAL, VERTICAL, GRID };
 
 typedef struct {
   double r, g, b;
@@ -34,28 +33,10 @@ inline void renderGrid(Viewport &viewport, HittableList &world,
   MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &cartComm);
   MPI_Comm_rank(cartComm, &rank);
 
-<<<<<<< HEAD
-  int coords[2];
-  MPI_Cart_coords(cartComm, rank, 2, coords);
-
-  int rowsPerProcess = imageHeight / size;
-  int remainder = imageHeight % size;
-  int startRow = rank * rowsPerProcess;
-  int numRows = rowsPerProcess + (rank < remainder ? 1 : 0);
-  int endRow = startRow + numRows;
-
-  std::vector<std::vector<Pixel>> localImage(endRow - startRow,
-                                             std::vector<Pixel>(imageWidth));
-
-  for (int i = startRow; i < endRow; i++)
-    for (int j = 0; j < viewport.imageWidth; j++) {
-      localImage[i - startRow][j] = viewport.getPixelColor(i, j, world);
-=======
-
   int numWorkers = size;
   int rowOffset = rank;
-  int numProcessedRows = imageHeight/numWorkers;
-  if (imageHeight%numWorkers > rowOffset) {
+  int numProcessedRows = imageHeight / numWorkers;
+  if (imageHeight % numWorkers > rowOffset) {
     numProcessedRows += 1;
   }
 
@@ -68,40 +49,36 @@ inline void renderGrid(Viewport &viewport, HittableList &world,
     img = (double *)malloc(sizeof(double) * imgSize);
   }
 
-  std::cout << "num workers: " << size << std::endl;
-  std::cout << "row offset: " << rank << std::endl;
-  std::cout << "num processed rows: " << numProcessedRows << std::endl;
-  std::cout << "local pixels size: " << imageWidth * numProcessedRows << std::endl;
-  std::cout << "total image size: " << imageHeight*imageWidth << std::endl;
-  
-  int currentPixel = 0;
-  for (int i = rowOffset; i < imageHeight; i += numWorkers) {
-    // std::cout << i << std::endl;
-    for (int j = 0; j < imageWidth; j++, currentPixel += 3) {
-      Pixel p = viewport.getPixelColor(i, j, world);
-      localPixels[currentPixel] = p.x;
-      localPixels[currentPixel+1] = p.y;
-      localPixels[currentPixel+2] = p.z;
-    }
-    // std::cout << "current pixel: " << currentPixel << std::endl;
-  }
-
-  int numBytesRead = MPI_Gather((void *)localPixels, localPixelsSize, MPI_DOUBLE, (void *)img, localPixelsSize, MPI_DOUBLE, 0, cartComm);
-
-  if (rank == 0) {
-    currentPixel = 0;
-    for (int i = 0; i < imageHeight; i++) {
+  if (loadBalanced) {
+    int currentPixel = 0;
+    for (int i = rowOffset; i < imageHeight; i += numWorkers) {
       for (int j = 0; j < imageWidth; j++, currentPixel += 3) {
-        image[i][j] = Pixel({
-          img[currentPixel],
-          img[currentPixel+1],
-          img[currentPixel+2]
-        });
-        // std::cout << image[i][j] << std::endl;
+        Pixel p = viewport.getPixelColor(i, j, world);
+        localPixels[currentPixel] = p.x;
+        localPixels[currentPixel + 1] = p.y;
+        localPixels[currentPixel + 2] = p.z;
       }
->>>>>>> refs/remotes/origin/main
+    }
+  } else {
+    for (int i = rowOffset; i < imageHeight; i += numWorkers)
+      for (int j = 0; j < imageWidth; j++) {
+        localPixels[i] = viewport.getPixelColor(i, j, world);
+      }
+
+    int numBytesRead =
+        MPI_Gather((void *)localPixels, localPixelsSize, MPI_DOUBLE,
+                   (void *)img, localPixelsSize, MPI_DOUBLE, 0, cartComm);
+
+    if (rank == 0) {
+      currentPixel = 0;
+      for (int i = 0; i < imageHeight; i++) {
+        for (int j = 0; j < imageWidth; j++, currentPixel += 3) {
+          image[i][j] = Pixel({img[currentPixel], img[currentPixel + 1],
+                               img[currentPixel + 2]});
+        }
+      }
+      toPpmFile(image, "test.ppm");
     }
 
-  toPpmFile(localImage, std::to_string(rank) + "test.ppm");
-  MPI_Comm_free(&cartComm);
-}
+    MPI_Comm_free(&cartComm);
+  }
